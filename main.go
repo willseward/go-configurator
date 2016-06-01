38e0a19b6f6b698cde14954c476c72e38e0b43fd
Wills Ward <wills.e.ward@tcu.edu>
2016-06-01 09:39:30 -0500

    bumped version

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
     
     app.Name = "Docker Auditd Exporter - Host Agent"
     app.Usage = "Exports the audit reports from containers to an API"
-    app.Version = "0.2"
+    app.Version = "0.3"
     
     SetupCliForConfigurator(app)
     // SetupCliForServer(app)
